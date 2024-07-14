b522d949d028abcfa80202fb56b652c1987ab7e7
Navinkumar Singh <148623513+navinsys@users.noreply.github.com>
2024-07-14 16:56:40 +0530

    Add files via upload

---
+++ 2nd .js
@@ -0,0 +1,59 @@
+//ACTIVITY 1
+//Task 1
+let num1 = 2;
+let num2 = 5;
+let sum = num1 + num2;
+//console.log(sum)
+
+
+//Task2 
+
+let difference = num1 - num2;
+//console.log(difference)
+
+
+//Task 3
+let mul = num1 * num2;
+//console.log(mul)
+
+
+//Task4 
+let div = num1 / num2;
+//console.log(div)
+
+//Task5
+let rem = num1 % num2;
+//console.log(rem)
+
+//ACTIVITY 2
+//Task6
+let num_0 = 2;
+num_0 += num1
+//console.log(num_0)
+
+//Task7
+num_0 -= num1
+//console.log(num_0)
+
+//ACTIVITY3
+//Task 8
+const age = 20;
+const isBada = age >= 20 ? true : false;
+
+const isChota = age < 20 ? true : false;
+
+const is20completed = age === 20 ? true : false;
+//console.log(isBada);
+//console.log(isChota);
+//console.log(is20completed);
+
+//ACTIVITY4
+let gender = "male";
+let role = "Frontend  developer";
+const isAdultMale = gender === "male" && age >= 20 ? true : false;
+const isTeens = age >= 12 || age < 20;
+const isFemale = !(gender === "male")
+
+console.log(isAdultMale)
+console.log(isTeens)
+console.log(isFemale)
